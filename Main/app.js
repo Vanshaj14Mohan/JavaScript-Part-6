bd72d091805200ef7318dd5cea6c937091951f77
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-25 23:28:05 +0530

    files

--- Main/app.js
+++ Main/app.js
@@ -1,7 +1,13 @@
+//This keyword refers to an object that is executing the current piece of code.
 const student = {
-    name: "Vanshaj", 
+    name: "John", 
     age: 22,
     eng: 92,
     maths: 95,
-    computer: 95
+    computer: 95,
+    getAvg() { //method/ functions can't directly access our parameters. 
+        console.log(this);
+        avg = (this.eng + this.maths + this.computer) / 3; //used this as 
+        console.log(`${this.name} got average marks = ${avg}`);
+    }
 }
