3e8e2053f5b77df6918eee7c241e142e7e814cf7
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-26 23:43:11 +0530

    files

--- Try_catch/app.js
+++ Try_catch/app.js
@@ -1,8 +1,16 @@
 //Try & Catch statement
 //The try statement allows you to define a block of code to be tested for errors while it being executed.
 //The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
+console.log("Hello 1");
+console.log("Hello 2");
+// console.log(a);
+try{
+    console.log(a);
+} catch(err){
+    console.log("Error caught, a not defined");
+    console.log(err); //will tell us about our error line
+}
+console.log("Hello 3");
+console.log("Hello 4");
 
-
-
-
-console.log(a);
+//So we use try & catch when we are not sure about a code giving error or not.
