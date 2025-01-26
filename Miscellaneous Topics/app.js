141eb29b9ca2d65fc5b36eaf0ab9bf0f116f124e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-27 00:16:42 +0530

    Arrow functions

---
+++ Miscellaneous Topics/app.js
@@ -0,0 +1,15 @@
+const sum = (a,b) =>{
+    console.log(a+b);
+}
+
+const cube = (n) =>{
+    return n*n*n;
+}
+
+const pow = (a,b) =>{
+    return Math.pow(a,b);
+    // or return a**b; both will work fine:
+}
+
+//Note -> If an arrow function has an single argument in it, then we can remove the parentheses around the argument. 
+//Applicable only in single argument case.
