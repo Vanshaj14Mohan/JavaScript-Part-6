2bfca105159afe04eb460ad436e03c6be6b20755
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-26 23:33:05 +0530

    files

---
+++ Try_catch/app.js
@@ -0,0 +1,8 @@
+//Try & Catch statement
+//The try statement allows you to define a block of code to be tested for errors while it being executed.
+//The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
+
+
+
+
+console.log(a);
