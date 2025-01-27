528e2bab175e2458a346bd2d228f80f69ab63b83
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-27 18:14:49 +0530

    Implicit return in arrow functions

--- Miscellaneous Topics/app.js
+++ Miscellaneous Topics/app.js
@@ -13,3 +13,10 @@ const pow = (a,b) =>{
 
 //Note -> If an arrow function has an single argument in it, then we can remove the parentheses around the argument. 
 //Applicable only in single argument case.
+
+//Now arrow functions can do Implicit return of data ie-> if we don't use return keyword then the data will be returned automatically.
+// eg-> replace {} with ().
+const mul = (a,b) =>(
+    a*b
+)
+//Not necessary to write like this, it's just a syntax we should know.
