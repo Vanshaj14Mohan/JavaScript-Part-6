c75532faf33e4e4db815747b0df1484b8c89e506
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-29 23:49:59 +0530

    files

--- Main/Main Two/app.js
+++ Main/Main Two/app.js
@@ -8,7 +8,17 @@ const student = {
     },
     //Now using same thing with arrow functions
     getMarks: () => {
-        console.log(this); //this would be student object, ie-> parent's scope
+        console.log(this); //this would be student object, ie-> parent's scope = window
         return this.marks; //will show undefined
-    }
+    }, 
+    getInfo1: function(){
+        setTimeout( ()=>{
+            console.log(this); //this would be student object here.
+        }, 2000);
+    },
+    getInfo2: function(){ //using a normal function 
+        setTimeout(function (){
+            console.log(this); //this would be Window object here
+        },2000);
+    },
 };
