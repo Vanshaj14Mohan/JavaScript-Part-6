2c9b3c33e3edb038bcf9eab0a935c13abe07619e
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-30 23:55:23 +0530

    Practice Set

--- Practice Set/app.js
+++ Practice Set/app.js
@@ -1,4 +1,14 @@
-//Write an arrow function that return the sqaure of a number;
+//1. Write an arrow function that return the sqaure of a number;
 const num = (n) =>{
     return n * n;
 }
+
+//2. Write a function that prints "Hello world" 5 times at an interval of 2s each.
+let id = setInterval(() =>{
+    console.log("Hello world");
+}, 2000);
+
+setTimeout(()=>{
+    clearInterval(id);
+    console.log("Interval ran clearly")
+},10000);
