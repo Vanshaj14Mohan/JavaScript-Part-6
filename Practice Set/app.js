aa43f138558fd8c6c8270c6c5dbeae7ed4a22896
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-30 23:58:45 +0530

    Practice Set

--- Practice Set/app.js
+++ Practice Set/app.js
@@ -6,9 +6,9 @@ const num = (n) =>{
 //2. Write a function that prints "Hello world" 5 times at an interval of 2s each.
 let id = setInterval(() =>{
     console.log("Hello world");
-}, 2000);
+}, 2000); //2s time
 
 setTimeout(()=>{
     clearInterval(id); //to exit the case
     console.log("Interval ran clearly") //checking if the interval is cleared
-},10000);
+},10000); //exit time 1s = 1000ms so, 10s = 10000ms
