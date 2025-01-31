8d28584520e954b8f610413b17fb256dbd6e7183
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-31 23:54:53 +0530

    Assignment

--- Assignments/app.js
+++ Assignments/app.js
@@ -1,5 +1,22 @@
-//Write an arrow function named isEven(), that takes a single number as argument and returns if it is even or not.
+//1.Write an arrow function named isEven(), that takes a single number as argument and returns if it is even or not.
 let num = 4;
 const isEven = (num) =>{
     return num % 2 === 0;
 }
+
+//2. Verify the output.
+let length = 4;
+function callback(){
+    console.log(this.length);
+}
+
+const object ={
+    length: 5,
+    method(callback){
+        callback();
+    },
+}
+
+object.method(callback, 1, 2);
+
+//3
