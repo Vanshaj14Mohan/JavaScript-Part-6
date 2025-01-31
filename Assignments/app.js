16284802296f4e0c077352320ebf4feb141a2cb1
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-31 23:58:40 +0530

    Assignments

--- Assignments/app.js
+++ Assignments/app.js
@@ -19,4 +19,14 @@ const object ={
 
 object.method(callback, 1, 2);
 
-//3
+//3. Write an arrow function named arrayAverage that accepts an array of numbers and returns the average of those numbers.
+const arrayAverage = (arr) =>{
+    let total = 0;
+    for(let number of arr){
+        total = total + number;
+    }
+    return total / arr.length;
+};
+
+let arr = [1,2,3,4,5]
+console.log(arrayAverage(arr)); 
